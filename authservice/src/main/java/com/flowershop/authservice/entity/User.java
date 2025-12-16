28545fdb3617788f066d55965f490917418ef27f
Lacky227 <tishko.artyr@gmail.com>
2025-12-16 13:41:16 +0200

    feat(authservice-entity): add User entity and AuthProvider, Role enums

---
+++ authservice/src/main/java/com/flowershop/authservice/entity/User.java
@@ -0,0 +1,51 @@
+package com.flowershop.authservice.entity;
+
+import com.flowershop.authservice.entity.emums.AuthProvider;
+import com.flowershop.authservice.entity.emums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname", nullable = false,  length = 100)
+    private String firstname;
+
+    @Column(name = "lastname", length = 100)
+    private String lastname;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 128)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "auth_provider", nullable = false, length = 20)
+    private AuthProvider authProvider;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 20)
+    private Role role;
+
+    @Column(name = "is_marketing_allowed", nullable = false)
+    private Boolean isMarketingAllowed;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ authservice/src/main/java/com/flowershop/authservice/entity/emums/Role.java
@@ -0,0 +1,5 @@
+package com.flowershop.authservice.entity.emums;
+
+public enum Role {
+    USER, ADMIN
+}
