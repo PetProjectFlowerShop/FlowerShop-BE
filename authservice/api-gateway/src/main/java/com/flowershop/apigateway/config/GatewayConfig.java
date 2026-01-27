30ca8b6dc48b2f546e98817e0f8946f2a797b07b
smagles <lesialist@icloud.com>
2026-01-27 20:40:44 +0100

    feat: add gateway routing

--- authservice/api-gateway/src/main/java/com/flowershop/apigateway/config/GatewayConfig.java
+++ authservice/api-gateway/src/main/java/com/flowershop/apigateway/config/GatewayConfig.java
@@ -1,16 +1,27 @@
 package com.flowershop.apigateway.config;
 
+import com.flowershop.apigateway.filters.AccessFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class GatewayConfig {
+    private final AccessFilter accessFilter;
 
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        // TODO: add routes to services using Eureka (registry-service) name uri
-        return null;
+        return builder.routes()
+            .route("auth-service", r -> r.path("/auth/**")
+                .uri("lb://auth-service"))
+            .route("catalog-service", r -> r.path("/flowers/**")
+                .filters(f -> f.filter(accessFilter))
+                .uri("lb://catalog-service"))
+            .route("order-service", r -> r.path("orders/**")
+                .filters(f -> f.filter(accessFilter))
+                .uri("lb://order-service")).build();
     }
 }
