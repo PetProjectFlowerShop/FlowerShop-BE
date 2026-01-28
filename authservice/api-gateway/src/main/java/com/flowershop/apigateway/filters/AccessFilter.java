0b00ec1f7dc8f68379adcac1fc593fa1a8dabe8a
Olesia Smahlii <lesialist@icloud.com>
2026-01-28 08:56:07 +0100

    feat(api-gateway): configure routing, CORS, and JWT security
    - Configured API Gateway routing to backend services.
    - Added CORS configuration for cross-origin requests.
    - Secured gateway with JWT access filter.

--- authservice/api-gateway/src/main/java/com/flowershop/apigateway/filters/AccessFilter.java
+++ authservice/api-gateway/src/main/java/com/flowershop/apigateway/filters/AccessFilter.java
@@ -1,8 +1,11 @@
 package com.flowershop.apigateway.filters;
 
+import com.flowershop.apigateway.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -10,9 +13,32 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class AccessFilter implements GatewayFilter {
-    // TODO: add access filter configuration what checking header and token valid
+    private final JwtService jwtService;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        String authHeader = exchange.getRequest()
+            .getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return onError(exchange, HttpStatus.UNAUTHORIZED);
+        }
+
+        String token = authHeader.substring(7);
+
+        if (!jwtService.isValidToken(token)) {
+            return onError(exchange, HttpStatus.UNAUTHORIZED);
+        }
+        String email = jwtService.extractEmail(token);
+
+        return chain.filter(
+            exchange.mutate()
+                .request(r -> r.header("X-User-Email", email))
+                .build());
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
+        exchange.getResponse().setStatusCode(status);
+        return exchange.getResponse().setComplete();
     }
 }
