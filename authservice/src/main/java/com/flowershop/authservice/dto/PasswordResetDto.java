1821824ff15288d8da65cf08eb2b37a8d0aa4d80
Eugene L <lugovoj20@gmail.com>
2026-02-18 11:57:59 +0100

    refactor: change password reset token delivery to URL path

--- authservice/src/main/java/com/flowershop/authservice/dto/PasswordResetDto.java
+++ authservice/src/main/java/com/flowershop/authservice/dto/PasswordResetDto.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record PasswordResetDto(
-    @NotBlank(message = "Token is required")
-    String token,
     @NotBlank(message = "Password is required")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
         message = "Password must be at least 8 characters long, contain at least one uppercase letter and one digit")
--- authservice/src/main/java/com/flowershop/authservice/service/PasswordRecoveryService.java
+++ authservice/src/main/java/com/flowershop/authservice/service/PasswordRecoveryService.java
@@ -7,6 +7,7 @@ import com.flowershop.authservice.exceptions.TokenNoValidException;
 import com.flowershop.authservice.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class PasswordRecoveryService {
     private final UserRepository userRepository;
     private final PasswordEncoder bCryptPasswordEncoder;
     private final StringRedisTemplate redisTemplate;
+    @Value("${app.password.recovery.host}")
+    private String host;
+    @Value("${app.password.recovery.path}")
+    private String recoveryPath;
 
     public void requestPasswordRecovery(String email){
         if(!userRepository.existsByEmail(email)){
@@ -27,19 +32,17 @@ public class PasswordRecoveryService {
         }
 
         String token = UUID.randomUUID().toString();
-        System.out.println("Token = " + token);
+        String url = host + recoveryPath + token;
+        System.out.println("url = " + url);
         redisTemplate.opsForValue().set(token, email, Duration.ofMinutes(15));
     }
 
     @Transactional
-    public void confirmPassword(PasswordResetDto passwordResetDto){
-        String token = passwordResetDto.token();
+    public void confirmPassword(String token, PasswordResetDto passwordResetDto){
         String newPassword = passwordResetDto.newPassword();
         String email = redisTemplate.opsForValue().getAndDelete(token);
-
         if (email == null){
             throw new TokenNoValidException();
-
         }
 
         User user = userRepository.findUserByEmail(email);
