58c6d4062bafc387d62c441a47af8c0b9fcb01d5
Lacky227 <tishko.artyr@gmail.com>
2026-02-25 23:46:53 +0200

    feat(auth): implement Google OAuth login and registration flow

---
+++ authservice/src/main/java/com/flowershop/authservice/service/GoogleAuthService.java
@@ -0,0 +1,59 @@
+package com.flowershop.authservice.service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.flowershop.authservice.dto.GoogleLoginOrRegisterDTO;
+import com.flowershop.authservice.exceptions.InvalidGoogleTokenException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.Value;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleAuthService {
+    @Value("${google.clientId}")
+    private final String clientId;
+
+    public GoogleLoginOrRegisterDTO loginWithGoogle(String credential) {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
+                new NetHttpTransport(),
+                new GsonFactory()).setAudience(List.of(clientId))
+                .build();
+
+        GoogleIdToken idToken;
+        try {
+            idToken = verifier.verify(credential);
+        } catch (GeneralSecurityException | IOException e) {
+            throw new InvalidGoogleTokenException("Unauthorized");
+        }
+        if (idToken == null) {
+            throw new InvalidGoogleTokenException("Unauthorized");
+        }
+
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        String providerId = payload.getSubject();
+        String email = payload.getEmail();
+        String givenName = (String) payload.getOrDefault("given_name", "");
+        String familyName = (String) payload.getOrDefault("family_name", "");
+        Boolean emailVerified = payload.getEmailVerified();
+
+        if (!Boolean.TRUE.equals(emailVerified)) {
+            throw new InvalidGoogleTokenException("Unauthorized");
+        }
+
+        return new GoogleLoginOrRegisterDTO(
+                providerId,
+                givenName,
+                familyName,
+                email);
+    }
+
+}
--- authservice/src/main/java/com/flowershop/authservice/service/UserService.java
+++ authservice/src/main/java/com/flowershop/authservice/service/UserService.java
@@ -1,6 +1,7 @@
 package com.flowershop.authservice.service;
 
 import com.flowershop.authservice.dto.AuthResponse;
+import com.flowershop.authservice.dto.GoogleLoginOrRegisterDTO;
 import com.flowershop.authservice.dto.RegisterRequest;
 import com.flowershop.authservice.entity.constrains.ApiErrorMessage;
 import com.flowershop.authservice.dto.LoginRequest;
@@ -10,55 +11,79 @@ import com.flowershop.authservice.entity.emums.AuthProvider;
 import com.flowershop.authservice.entity.emums.Role;
 import com.flowershop.authservice.exceptions.BadCredentialsException;
 import com.flowershop.authservice.exceptions.EmailAlreadyExistsException;
+import com.flowershop.authservice.exceptions.InvalidGoogleTokenException;
 import com.flowershop.authservice.exceptions.NotFoundException;
 import com.flowershop.authservice.repository.UserRepository;
 import com.flowershop.authservice.utils.JwtUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 @RequiredArgsConstructor
-public class UserService  {
+public class UserService {
     private final JwtUtils jwtUtils;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-
+    private final GoogleAuthService googleAuthService;
 
     @Transactional
     public AuthResponse register(RegisterRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
             throw new EmailAlreadyExistsException(
-                String.format("Email '%s' is already registered",
-                    request.getEmail())
-            );
+                    String.format("Email '%s' is already registered",
+                            request.getEmail()));
         }
 
         User user = User.builder()
-            .firstname(request.getFirstName())
-            .lastname(request.getLastName())
-            .email(request.getEmail())
-            .password(passwordEncoder.encode(request.getPassword()))
-            .authProvider(AuthProvider.LOCAL)
-            .role(Role.ROLE_USER)
-            .isMarketingAllowed(request.getIsMarketingAllow())
-            .build();
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .authProvider(AuthProvider.LOCAL)
+                .role(Role.ROLE_USER)
+                .isMarketingAllowed(request.getIsMarketingAllow())
+                .build();
         userRepository.save(user);
         return new AuthResponse(jwtUtils.generateToken(user.getEmail()));
     }
 
     public LoginResponseDto login(LoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
-            .orElseThrow(() ->
-                new NotFoundException(ApiErrorMessage.USER_NOT_FOUND_BY_EMAIL.getMessage(request.getEmail())));
+                .orElseThrow(() -> new NotFoundException(
+                        ApiErrorMessage.USER_NOT_FOUND_BY_EMAIL.getMessage(request.getEmail())));
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new BadCredentialsException(ApiErrorMessage.INVALID_PASSWORD.getMessage(request.getEmail()));
         }
         String jwtToken = jwtUtils.generateToken(request.getEmail());
         return new LoginResponseDto(jwtToken, user.getRole().name());
 
+    }
+
+    @Transactional
+    public AuthResponse loginOrRegisterWithGoogle(String credential) {
+        GoogleLoginOrRegisterDTO request = googleAuthService.loginWithGoogle(credential);
+        if (request == null) {
+            throw new InvalidGoogleTokenException("Unauthorized");
+        }
+        Optional<User> user = userRepository.findByProviderIdAndEmail(request.providerId(), request.email());
+        if (user.isPresent()) {
+            return new AuthResponse(jwtUtils.generateToken(user.get().getEmail()));
+        }
+        User newUser = User.builder()
+                .firstname(request.firstName())
+                .lastname(request.lastName())
+                .email(request.email())
+                .providerId(request.providerId())
+                .authProvider(AuthProvider.GOOGLE)
+                .role(Role.ROLE_USER)
+                .isMarketingAllowed(true)
+                .build();
+        userRepository.save(newUser);
+        return new AuthResponse(jwtUtils.generateToken(newUser.getEmail()));
     }
 }
