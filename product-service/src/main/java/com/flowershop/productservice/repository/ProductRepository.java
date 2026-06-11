d4c3fa17c76cf813c8b86153b25633f6d3e2f271
smagles <lesialist@icloud.com>
2026-06-11 17:42:36 +0200

    fix: problem n+1 with entity graph

--- product-service/src/main/java/com/flowershop/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/flowershop/productservice/repository/ProductRepository.java
@@ -4,11 +4,13 @@ import com.flowershop.productservice.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    @EntityGraph(attributePaths = {"images", "colors", "occasions", "flowerTypes"})
     Page<Product> findAll(Specification<Product> spec, Pageable page);
 }
