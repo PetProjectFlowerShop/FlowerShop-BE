39d3183eb60a1cffc20e3a8b507430fcc57db5c9
Ruslan Zapolianenko <rusartrade@gmail.com>
2026-06-12 23:49:22 +0300

    fixed bag 2

--- product-service/src/main/java/com/flowershop/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/flowershop/productservice/ProductServiceApplication.java
@@ -3,6 +3,8 @@ package com.flowershop.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
