df6ffb60750cde3f46157b9b863dcf96e31ae6b7
smagles <lesialist@icloud.com>
2026-06-19 15:18:20 +0200

    refactor: move product recommendation logic

--- product-service/src/main/java/com/flowershop/productservice/controller/CatalogController.java
+++ product-service/src/main/java/com/flowershop/productservice/controller/CatalogController.java
@@ -2,11 +2,14 @@ package com.flowershop.productservice.controller;
 
 import com.flowershop.productservice.dto.ProductFilterRequest;
 import com.flowershop.productservice.dto.ProductFilterResponse;
+import com.flowershop.productservice.dto.ProductRecommendResponse;
+import com.flowershop.productservice.service.ProductRecommendService;
 import com.flowershop.productservice.service.catalog.CatalogService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -14,6 +17,7 @@ import java.util.List;
 @RequestMapping("/flowers")
 public class CatalogController {
     private final CatalogService service;
+    private final ProductRecommendService productRecommendService;
 
     @GetMapping("/search")
     public Page<ProductFilterResponse> getAllNeeded(@Valid ProductFilterRequest request) {
@@ -23,5 +27,10 @@ public class CatalogController {
     @GetMapping("/batch")
     public List<ProductFilterResponse> getProductsByIds(@RequestParam List<Long> ids) {
         return service.getProductsByIds(ids);
+    }
+
+    @GetMapping("/recommendations")
+    public List<ProductRecommendResponse> getRecommendations() {
+        return productRecommendService.getRecommendations();
     }
 }
