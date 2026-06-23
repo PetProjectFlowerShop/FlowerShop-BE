2747de5850df024d38076c224d5f562f24bbd07b
Olesia Smahlii <lesialist@icloud.com>
2026-06-23 19:23:07 +0200

    Delete product-service/src/main/java/com/flowershop/productservice/service/ProductRecommendService.java

--- product-service/src/main/java/com/flowershop/productservice/service/ProductRecommendService.java
+++
@@ -1,8 +0,0 @@
-package com.flowershop.productservice.service;
-
-import com.flowershop.productservice.dto.ProductRecommendResponse;
-import java.util.List;
-
-public interface ProductRecommendService {
-    List<ProductRecommendResponse> getRecommendations();
-}
