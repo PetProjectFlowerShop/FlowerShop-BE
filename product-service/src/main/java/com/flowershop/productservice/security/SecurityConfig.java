6185a0492f6b32fcc6dac0866b86dc21bb0314b8
smagles <lesialist@icloud.com>
2026-06-28 15:38:52 +0200

    fear: add spring security to product service

---
+++ product-service/src/main/java/com/flowershop/productservice/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.flowershop.productservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final HeaderAuthenticationFilter filter;
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll())
+                .build();
+    }
+}
