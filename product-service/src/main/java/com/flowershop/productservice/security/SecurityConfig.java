e18820fedb0cccf58e7a7ac5013eb02bd8ff94e7
smagles <lesialist@icloud.com>
2026-08-02 18:22:42 +0200

    refactor: update cors configuration

--- product-service/src/main/java/com/flowershop/productservice/security/SecurityConfig.java
+++ product-service/src/main/java/com/flowershop/productservice/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.flowershop.productservice.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,7 +17,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http.csrf(AbstractHttpConfigurer::disable)
-            .cors(Customizer.withDefaults())
+            .cors(AbstractHttpConfigurer::disable)
             .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll())
